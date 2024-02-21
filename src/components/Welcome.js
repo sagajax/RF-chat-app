c6082cd90c630427afb938cf097aa0fa4ac0db95
sagajax <129790355+sagajax@users.noreply.github.com>
2024-02-22 00:33:28 +0530

    Welcome changed

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,9 +1,14 @@
+import { auth } from "../firebase";
+import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth"
+
 import React from "react";
 import GoogleSignin from "../img/btn_google_signin_dark_pressed_web.png";
 
 const Welcome = () => {
   const googleSignIn = () => {
-  };
+    const provider = new GoogleAuthProvider();
+    signInWithRedirect(auth, provider);
+};
 
   return (
     <main className="welcome">
