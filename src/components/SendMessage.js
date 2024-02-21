5de3f2b81f78faca261665e4f45fdcdeaa3bebd3
sagajax <129790355+sagajax@users.noreply.github.com>
2024-02-22 01:20:38 +0530

    Send message setup

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { auth, db } from "../firebase";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 
 const SendMessage = () => {
+  const [message, setMessage] = useState("");
+  const sendMessage = async (event) => {
+    event.preventDefault();
+    if (message.trim() === "") {
+      alert("Enter valid message");
+      return;
+    }
+    const { uid, displayName, photoURL } = auth.currentUser;
+    await addDoc(collection(db, "messages"), {
+      text: message,
+      name: displayName,
+      avatar: photoURL,
+      createdAt: serverTimestamp(),
+      uid,
+    });
+    setMessage("");
+  };
+
   return (
-    <form className="send-message">
+    <form  onSubmit={(event) => sendMessage(event)} className="send-message">
       <label htmlFor="messageInput" hidden>
         Enter Message
       </label>
@@ -16,6 +36,8 @@ const SendMessage = () => {
       <button type="submit">Send</button>
     </form>
   );
+
 };
+
 
 export default SendMessage;
