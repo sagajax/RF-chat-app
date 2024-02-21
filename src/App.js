01e09c06ffd6a7f40b654105a5d6be966607c836
sagajax <129790355+sagajax@users.noreply.github.com>
2024-02-22 00:27:06 +0530

    firebase integrated

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import ChatBox from "./components/ChatBox";
 import Welcome from "./components/Welcome";
-import { useState } from "react";
+import { auth } from "./firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 function App() {
-  const [user, setUser] = useState(false);
+  const [user] = useAuthState(auth);
 
   return (
     <div className="App">
